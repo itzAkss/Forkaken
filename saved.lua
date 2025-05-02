87acf7aad1b571177f4ce3da616bebd13fd4323c
itzAkss <akshahanaisultan97@gmail.com>
2025-05-02 14:58:31 +0500

    Update saved.lua

--- saved.lua
+++ saved.lua
@@ -1,18 +1,19 @@
--- Load the Luarmor library
-local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
-
--- Set the script ID (you must know it; sometimes provided by the script owner)
-api.script_id = "d0632c30c8af0316aae11552dcb2a21d"
-
--- Provide your key
-local key = "wDENXIqPMcgUpPjfLVBMdAWJbeNwTIkO"
-
--- Check the key and load the script
-local response = api["check_key"](key)
-
-if response and response.code == "KEY_VALID" then
-    print("Key is valid. Loading script...")
-    api["load_script"]()
-else
-    warn("Invalid key: " .. (response.message or "Unknown error"))
+-- Ni gg Er!
+local oldLoadstring = loadstring
+_G.loadstring = function(source, ...)
+    print("----- Luarmor Script Start -----\n")
+    print(source)
+    print("\n----- Luarmor Script End -----")
+    return function() end
 end
+
+-- key n id
+local key = "wDENXIqPMcgUpPjfLVBMdAWJbeNwTIkO"
+local script_id = "d0632c30c8af0316aae11552dcb2a21d"
+
+-- Load LuarmorLib
+local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
+api.script_id = script_id
+
+api.check_key(key)
+api.load_script()
